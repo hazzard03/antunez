49271ca8581ee4c99aac369845d0a8c4e0089f50
hazzard03 <116839698+hazzard03@users.noreply.github.com>
2024-11-28 14:04:15 -0500

    sem produtos

---
+++ js/buttom.js
@@ -0,0 +1,5 @@
+function scrollCards() {
+    const container = document.querySelector('.card-container');
+    container.scrollBy({
+        left: 300, // Ajuste a quantidade de rolagem
+        behavior: '
